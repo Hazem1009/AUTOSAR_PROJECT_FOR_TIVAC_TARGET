f2da743bd0eb873ae22978e68eec62a9dc9d9529
Hazem Hisham <hazemhesham10920@gmail.com>
2022-04-16 02:16:36 +0200

    Added separate file for Port Driver

--- AUTOSAR_Project/Det.c
+++ AUTOSAR_Project/Det.c
@@ -10,6 +10,11 @@
  ******************************************************************************/
 
 #include "Det.h"
+/* Variables to store last DET error */
+uint16 Det_ModuleId = 0;       /*DET module ID*/
+uint8 Det_InstanceId = 0;      /*DET instance ID*/
+uint8 Det_ApiId = 0;           /* DET API ID*/
+uint8 Det_ErrorId = 0;         /* DET Error ID*/
 
 Std_ReturnType Det_ReportError( uint16 ModuleId,
                       uint8 InstanceId,
